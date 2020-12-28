12b96618513111f2fb910fa2e1ac482f89390d3d
liuxiu1 <1039544668@qq.com>
2020-12-28 18:20:30 +0800

    拆分地址的js

--- pages/personal/address/viewModules/mobile.vue
+++ pages/personal/address/viewModules/mobile.vue
@@ -24,19 +24,22 @@
             <span>Please fill in the information below</span>
             <i class="el-icon-close" @click="() => formShowDeal(false)"></i>
           </div>
-          <Form
+          <cup-address-form
             class="form"
             :data="formData"
             :is-edit="isEdit"
-            :ref="'formData'"
-          />
-          <div class="default-adress" @click="defaultAdressChoose">
-            <div
+            ref="formData"
+          ></cup-address-form>
+          <!-- <div class="default-adress" @click="defaultAdressChoose"> -->
+          <cup-checkbox v-model="defaultAdress">
+            <span>Set as default address</span>
+          </cup-checkbox>
+          <!-- <div
               class="checks"
               :class="formData.isDefault ? 'el-icon-check' : ''"
             ></div>
-            <span>Default adress</span>
-          </div>
+            <span>Default adress</span> -->
+          <!-- </div> -->
           <div
             class="submit_btn_container"
             @click="newOrUpdateAddress"
@@ -50,139 +53,9 @@
   </div>
 </template>
 <script>
-import List from '../components/mobileAdressList'
-import Form from '../../../../components/business/cupAddressForm'
+import addressMixin from '../addressMixin'
 export default {
-  components: {
-    List,
-    Form,
-  },
-  async asyncData({ app: { $http, $api }, query }) {},
-  data() {
-    return {
-      addOrEditAddress: true, // add true - edit false
-      isEdit: false,
-      formShow: false,
-      formTopAni: false,
-      defaultAdress: false,
-      adressList: [],
-      formData: {
-        addressFirst: null,
-        addressSecond: null,
-        alias: null,
-        city: null,
-        cityId: null,
-        company: null,
-        country: null,
-        countryId: null,
-        customerId: null,
-        firstName: null,
-        id: null,
-        isDefault: null,
-        lastName: null,
-        other: null,
-        postcode: null,
-        stateId: null,
-        stateName: null,
-        telephone: null,
-      },
-    }
-  },
-  mounted() {
-    this.getAddressList()
-  },
-  methods: {
-    /**
-     * 地址展示与隐藏
-     */
-    formShowDeal(status = false, addOrEditAddress = true) {
-      this.addOrEditAddress = addOrEditAddress
-      this.formShow = status
-      if (!status) {
-        this.formTopAni = status
-        this.formData = {
-          addressFirst: null,
-          addressSecond: null,
-          alias: null,
-          city: null,
-          cityId: null,
-          company: null,
-          country: null,
-          countryId: null,
-          customerId: null,
-          firstName: null,
-          id: null,
-          isDefault: null,
-          lastName: null,
-          other: null,
-          postcode: null,
-          stateId: null,
-          stateName: null,
-          telephone: null,
-        }
-      } else {
-        setTimeout(() => {
-          this.formTopAni = status
-        }, 0)
-      }
-    },
-    /**
-     * 默认地址设置
-     */
-    defaultAdressChoose() {
-      this.defaultAdress = !this.defaultAdress
-      this.formData.isDefault = this.formData.isDefault ? 0 : 1
-    },
-    /**
-     * 获取当前用户的所有地址
-     */
-    async getAddressList() {
-      const { list } = await this.$api.address.getAddressList()
-      this.adressList = list
-    },
-    /**
-     * 打开更新地址窗口
-     */
-    openUpdateDialog(formData) {
-      this.formData = { ...formData }
-      this.isEdit = true
-      this.formShowDeal(true, false)
-    },
-    /**
-     * 新增地址
-     * 更新地址
-     */
-    async newOrUpdateAddress() {
-      await (this.addOrEditAddress
-        ? this.$api.address.saveAddress({
-            ...this.$refs.formData.formData,
-            isDefault: this.formData.isDefault,
-          })
-        : this.$api.address.updateAddress({
-            ...this.$refs.formData.formData,
-            isDefault: this.formData.isDefault,
-          }))
-      this.getAddressList()
-      this.formShowDeal()
-      this.$alert({
-        text: '修改成功！',
-        isComfirm: true,
-      })
-    },
-    /**
-     * 删除地址
-     */
-    deleteAddress(id) {
-      this.$alert({
-        text: '是否确认删除？',
-        isCancel: true,
-        isComfirm: true,
-      }).then(async () => {
-        await this.$api.address.deleteAddress(id)
-        this.getAddressList()
-      })
-    },
-  },
+  mixins: [addressMixin],
 }
 </script>
 <style lang="scss" scoped>
