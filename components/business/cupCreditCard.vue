88dc2cea629e8cc1d53fef1f4fa56b953a3325cf
liuxiu1 <1039544668@qq.com>
2021-01-03 14:24:10 +0800

    信用卡支付样式修改

--- components/business/cupCreditCard.vue
+++ components/business/cupCreditCard.vue
@@ -250,6 +250,10 @@ export default {
         font-size: 14px;
         height: 44px;
         color: #999;
+        display: flex;
+        align-items: center;
+        box-sizing: border-box;
+        line-height: 18px;
         border-radius: 0;
         padding: 13px 12px;
         outline: none;
