19e28bafe7a89ec8a221550febbee380c06b8d68
bearliu1991 <1070507176@qq.com>
2021-01-06 08:58:00 +0800

    缩放问题

--- static/js/flexible.js
+++ static/js/flexible.js
@@ -1,150 +1,149 @@
+;(function (win, lib) {
+  const isMobile = () => {
+    const u = navigator.userAgent
+    return !!u.match(/AppleWebKit.*Mobile.*/) // 是否为移动终端
+  }
+  /* 可设置的变量 end   */
 
+  const doc = win.document
+  const docEl = doc.documentElement
+  let metaEl = doc.querySelector('meta[name="viewport"]')
+  const flexibleEl = doc.querySelector('meta[name="flexible"]')
+  let dpr = 0
+  let scale = 0
+  let tid
+  const flexible = lib.flexible || (lib.flexible = {})
 
-(function (win, lib) {
-    const isMobile = () => {
-        const u = navigator.userAgent
-        return !!u.match(/AppleWebKit.*Mobile.*/) // 是否为移动终端
+  if (metaEl) {
+    const match = metaEl
+      .getAttribute('content')
+      .match(/initial\-scale=([\d\.]+)/)
+    if (match) {
+      scale = parseFloat(match[1])
+      dpr = parseInt(1 / scale)
     }
-    /* 可设置的变量 end   */
-
-    const doc = win.document
-    const docEl = doc.documentElement
-    let metaEl = doc.querySelector('meta[name="viewport"]')
-    const flexibleEl = doc.querySelector('meta[name="flexible"]')
-    let dpr = 0
-    let scale = 0
-    let tid
-    const flexible = lib.flexible || (lib.flexible = {})
-
-    if (metaEl) {
-        const match = metaEl
-            .getAttribute('content')
-            .match(/initial\-scale=([\d\.]+)/)
-        if (match) {
-            scale = parseFloat(match[1])
-            dpr = parseInt(1 / scale)
-        }
-    } else if (flexibleEl) {
-        const content = flexibleEl.getAttribute('content')
-        if (content) {
-            const initialDpr = content.match(/initial\-dpr=([\d\.]+)/)
-            const maximumDpr = content.match(/maximum\-dpr=([\d\.]+)/)
-            if (initialDpr) {
-                dpr = parseFloat(initialDpr[1])
-                scale = parseFloat((1 / dpr).toFixed(2))
-            }
-            if (maximumDpr) {
-                dpr = parseFloat(maximumDpr[1])
-                scale = parseFloat((1 / dpr).toFixed(2))
-            }
-        }
+  } else if (flexibleEl) {
+    const content = flexibleEl.getAttribute('content')
+    if (content) {
+      const initialDpr = content.match(/initial\-dpr=([\d\.]+)/)
+      const maximumDpr = content.match(/maximum\-dpr=([\d\.]+)/)
+      if (initialDpr) {
+        dpr = parseFloat(initialDpr[1])
+        scale = parseFloat((1 / dpr).toFixed(2))
+      }
+      if (maximumDpr) {
+        dpr = parseFloat(maximumDpr[1])
+        scale = parseFloat((1 / dpr).toFixed(2))
+      }
     }
+  }
 
-    if (!dpr && !scale) {
-        const isAndroid = win.navigator.appVersion.match(/android/gi)
-        const isIPhone = win.navigator.appVersion.match(/iphone/gi)
-        const devicePixelRatio = win.devicePixelRatio
-        if (isIPhone) {
-            // iOS下，对于2和3的屏，用2倍的方案，其余的用1倍方案
-            if (devicePixelRatio >= 3 && (!dpr || dpr >= 3)) {
-                dpr = 3
-            } else if (devicePixelRatio >= 2 && (!dpr || dpr >= 2)) {
-                dpr = 2
-            } else {
-                dpr = 1
-            }
-        } else {
-            // 其他设备下，仍旧使用1倍的方案
-            dpr = 1
-        }
-        scale = 1 / dpr
-    }
-
-    docEl.setAttribute('data-dpr', dpr)
-    if (!metaEl) {
-        metaEl = doc.createElement('meta')
-        metaEl.setAttribute('name', 'viewport')
-        metaEl.setAttribute(
-            'content',
-            'initial-scale=' +
-                scale +
-                ', maximum-scale=' +
-                scale +
-                ', minimum-scale=' +
-                scale +
-                ', user-scalable=no'
-        )
-        if (docEl.firstElementChild) {
-            docEl.firstElementChild.appendChild(metaEl)
-        } else {
-            const wrap = doc.createElement('div')
-            wrap.appendChild(metaEl)
-            doc.write(wrap.innerHTML)
-        }
-    }
-
-    function refreshRem() {
-        let width = docEl.getBoundingClientRect().width
-        if(!isMobile()) {
-            width = width > 1200 ? width : 1200;
-        }
-        let rem;
-        if(isMobile()) {
-            rem = width / 3.75
-        } else {
-            rem = width / 19.2
-        }
-        docEl.style.fontSize = rem + 'px'
-        flexible.rem = win.rem = rem
-    }
-
-    win.addEventListener(
-        'resize',
-        function () {
-            clearTimeout(tid)
-            tid = setTimeout(refreshRem, 300)
-        },
-        false
-    )
-    win.addEventListener(
-        'pageshow',
-        function (e) {
-            if (e.persisted) {
-                clearTimeout(tid)
-                tid = setTimeout(refreshRem, 300)
-            }
-        },
-        false
-    )
-
-    if (doc.readyState === 'complete') {
-        doc.body.style.fontSize = 12 * dpr + 'px'
+  if (!dpr && !scale) {
+    const isAndroid = win.navigator.appVersion.match(/android/gi)
+    const isIPhone = win.navigator.appVersion.match(/iphone/gi)
+    const devicePixelRatio = win.devicePixelRatio
+    if (isIPhone) {
+      // iOS下，对于2和3的屏，用2倍的方案，其余的用1倍方案
+      if (devicePixelRatio >= 3 && (!dpr || dpr >= 3)) {
+        dpr = 3
+      } else if (devicePixelRatio >= 2 && (!dpr || dpr >= 2)) {
+        dpr = 2
+      } else {
+        dpr = 1
+      }
     } else {
-        doc.addEventListener(
-            'DOMContentLoaded',
-            function (e) {
-                doc.body.style.fontSize = 12 * dpr + 'px'
-            },
-            false
-        )
+      // 其他设备下，仍旧使用1倍的方案
+      dpr = 1
     }
+    scale = 1 / dpr
+  }
 
-    refreshRem()
+  docEl.setAttribute('data-dpr', dpr)
+  if (!metaEl) {
+    metaEl = doc.createElement('meta')
+    metaEl.setAttribute('name', 'viewport')
+    metaEl.setAttribute(
+      'content',
+      'initial-scale=' +
+        scale +
+        ', maximum-scale=' +
+        scale +
+        ', minimum-scale=' +
+        scale +
+        ', user-scalable=no'
+    )
+    if (docEl.firstElementChild) {
+      docEl.firstElementChild.appendChild(metaEl)
+    } else {
+      const wrap = doc.createElement('div')
+      wrap.appendChild(metaEl)
+      doc.write(wrap.innerHTML)
+    }
+  }
 
-    flexible.dpr = win.dpr = dpr
-    flexible.refreshRem = refreshRem
-    flexible.rem2px = function (d) {
-        let val = parseFloat(d) * this.rem
-        if (typeof d === 'string' && d.match(/rem$/)) {
-            val += 'px'
-        }
-        return val
+  function refreshRem() {
+    let width = docEl.getBoundingClientRect().width
+    if (!isMobile()) {
+      width = width > 1200 ? width : 1200
     }
-    flexible.px2rem = function (d) {
-        let val = parseFloat(d) / this.rem
-        if (typeof d === 'string' && d.match(/px$/)) {
-            val += 'rem'
-        }
-        return val
+    let rem
+    if (isMobile()) {
+      rem = width / 3.75
+    } else {
+      rem = width / 19.2
     }
+    docEl.style.fontSize = docEl.name === 'messageMobile' ? '100px' : rem + 'px'
+
+    flexible.rem = win.rem = rem
+  }
+
+  win.addEventListener(
+    'resize',
+    function () {
+      clearTimeout(tid)
+      tid = setTimeout(refreshRem, 300)
+    },
+    false
+  )
+  win.addEventListener(
+    'pageshow',
+    function (e) {
+      if (e.persisted) {
+        clearTimeout(tid)
+        tid = setTimeout(refreshRem, 300)
+      }
+    },
+    false
+  )
+
+  if (doc.readyState === 'complete') {
+    doc.body.style.fontSize = 12 * dpr + 'px'
+  } else {
+    doc.addEventListener(
+      'DOMContentLoaded',
+      function (e) {
+        doc.body.style.fontSize = 12 * dpr + 'px'
+      },
+      false
+    )
+  }
+
+  refreshRem()
+
+  flexible.dpr = win.dpr = dpr
+  flexible.refreshRem = refreshRem
+  flexible.rem2px = function (d) {
+    let val = parseFloat(d) * this.rem
+    if (typeof d === 'string' && d.match(/rem$/)) {
+      val += 'px'
+    }
+    return val
+  }
+  flexible.px2rem = function (d) {
+    let val = parseFloat(d) / this.rem
+    if (typeof d === 'string' && d.match(/px$/)) {
+      val += 'rem'
+    }
+    return val
+  }
 })(window, window.lib || (window.lib = {}))
